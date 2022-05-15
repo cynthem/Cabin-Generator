9b35f71bd56993ee398f6c576e47622eec709a11
doozles411 <cindy.phelps01@gmail.com>
2022-05-15 16:04:40 -0700

    Created Home route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/home/home';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/results">
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/home/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import cabinsIcon from '../../resources/cabinsIcon.jpg';
+
+export default function Home() {
+    return (
+        <div>
+            <p>Homepage</p>
+            <img src={cabinsIcon} alt="Row of various cabin styles"/>
+        </div>
+    )
+}
