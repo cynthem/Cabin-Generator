7fa0fdbc542886122bb10c4b38acdb0229153ca4
doozles411 <cindy.phelps01@gmail.com>
2022-05-16 15:17:01 -0700

    Created useScript custom hook

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import cabinsIcon from '../../resources/cabinsIcon.jpg';
-import coastJune17 from '../../resources/resultImages/coastImages/coastJune17.png';
+
+const styling = {
+    width: '450px', 
+    height: '300px', 
+    margin: 'auto'
+};
 
 export default function Results({ weekend, region }) {
     const imagesSelector = (weekend, region) => {
         if (weekend === "june17") {
             if (region === "coast") {
                 return (
-                    <img src={coastJune17} alt="Coast/Olympic June 17 result" />
+                    <div className="airbnb-embed-frame" data-id="506667266175518134" data-view="home" style={styling}>
+                        <a href="https://www.airbnb.com/rooms/506667266175518134?check_in=2022-06-17&amp;amp;check_out=2022-06-19&amp;amp;guests=1&amp;amp;adults=6&amp;amp;pets=1&amp;amp;s=66&amp;amp;source=embed_widget">View On Airbnb</a>
+                        <a href="https://www.airbnb.com/rooms/506667266175518134?check_in=2022-06-17&amp;amp;check_out=2022-06-19&amp;amp;guests=1&amp;amp;adults=6&amp;amp;pets=1&amp;amp;s=66&amp;amp;source=embed_widget" rel="nofollow">Happy Heron&amp;#x27;s Hideaway- dog friendly</a>
+                        <script async="" src="https://www.airbnb.com/embeddable/airbnb_jssdk"></script>
+                    </div>
                 )
             }
         }
