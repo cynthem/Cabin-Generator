2ce7bf9420c6889eed85601af327a65dcbc61f6b
doozles411 <cindy.phelps01@gmail.com>
2022-05-15 17:05:15 -0700

    Created radio buttons

--- src/components/home/map.js
+++ src/components/home/map.js
@@ -6,6 +6,20 @@ export default function Map() {
         <div>
             <p>Map</p>
             <img src={washingtonMap} alt="Map of Washington State" />
+            <div>
+                <input type="radio" id="coast" />
+                <label for="coast">Coast/Olympics</label>
+                <br/>
+                <input type="radio" id="cascades" />
+                <label for="cascades">Cascades</label>
+                <br/>
+                <input type="radio" id="central" />
+                <label for="central">Central Washington</label>
+                <br/>
+                <input type="radio" id="east" />
+                <label for="east">Eastside</label>
+                <br/>
+            </div>
         </div>
     )
 }
--- src/components/home/months.js
+++ src/components/home/months.js
@@ -5,7 +5,62 @@ export default function Months() {
     return (
         <div>
             <p>Months</p>
-            <img src={calendar} alt="June calendar" />
+            <div>
+                <img src={calendar} alt="June calendar" />
+                <div>
+                    <input type="radio" id="june17" />
+                    <label for="june17">June 17-19</label>
+                    <br/>
+                    <input type="radio" id="june24" />
+                    <label for="june24">June 24-26</label>
+                </div>
+            </div>
+            <div>
+                <img src={calendar} alt="August calendar" />
+                <div>
+                    <input type="radio" id="aug19" />
+                    <label for="aug19">August 19-21</label>
+                    <br/>
+                    <input type="radio" id="aug26" />
+                    <label for="aug26">August 26-28</label>
+                </div>
+            </div>
+            <div>
+                <img src={calendar} alt="September calendar" />
+                <div>
+                    <input type="radio" id="sept2" />
+                    <label for="sept2">September 2-4</label>
+                    <br/>
+                    <input type="radio" id="sept9" />
+                    <label for="sept9">September 9-11</label>
+                    <br/>
+                    <input type="radio" id="sept16" />
+                    <label for="sept16">September 16-18</label>
+                    <br/>
+                    <input type="radio" id="sept23" />
+                    <label for="sept23">September 23-25</label>
+                    <br/>
+                    <input type="radio" id="sept30" />
+                    <label for="sept30">Sept 30 - Oct 2</label>
+                </div>
+            </div>
+            <div>
+                <img src={calendar} alt="October calendar" />
+                <div>
+                    <input type="radio" id="oct7" />
+                    <label for="oct7">October 7-9</label>
+                    <br/>
+                    <input type="radio" id="oct14" />
+                    <label for="oct14">October 14-16</label>
+                    <br/>
+                    <input type="radio" id="oct21" />
+                    <label for="oct21">October 21-23</label>
+                    <br/>
+                    <input type="radio" id="oct28" />
+                    <label for="oct28">October 28-30</label>
+                </div>
+            </div>
+     
         </div>
     )
 }
